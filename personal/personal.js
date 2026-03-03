f3d1701d9891d6211a81a29bb5d30107e181f235
Meenu parashar <meenuparashar18@gmail.com>
2026-03-03 21:23:52 +0530

    FS module rivision

--- personal/personal.js
+++ personal/personal.js
@@ -300,19 +300,141 @@
 // server.listen(3000, () => {
 //   console.log('Server running on port 3000');
 // });
-const http = require('http');
-const fs = require('fs');
-
-http.createServer((req, res) => {
-  fs.appendFile(
-    'log.txt',
-    `${new Date().toISOString()} | ${req.method} | ${req.url}\n`,
-    console.error
-  );
-
-  res.end('Logged');
-}).listen(3000);
 
 
-const http = require ('http');
-const fs = require('fs'); 
+
+
+
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+
+// http.createServer((req, res) => {
+//   fs.appendFile(
+//     'log.txt',
+//     `${new Date().toISOString()} | ${req.method} | ${req.url}\n`,
+//     console.error
+//   );
+
+//   res.end('Logged');
+// }).listen(3000);
+
+
+// const http = require ('http');
+// const fs = require('fs'); 
+
+
+
+
+
+
+// aaj se rivision jo bhi padha h exam ke liye 9 march mid term h 
+// const http = require("http");
+// const server = http.createServer((req , res)=>{
+//   res.end("hello meenu");
+// });
+// server.listen(3000,()=>{
+//   console.log("server running on http://localhost:3000");
+// });
+
+
+
+// sabse pahle we want to study FS module
+//ak file banayi data.txt ye us file ka content padh rha h 
+
+// const fs = require("fs");
+// fs.readFile("data.txt","utf-8",(err, data)=>{
+//   if(err){
+//     console.log("Error:",err);
+//   }else{
+//     console.log("File content:",data);
+//   }
+// });
+
+
+
+
+
+//write file ye purani file ka content replace kr deta h
+
+// const fs=require("fs");
+// fs.writeFile("notes.txt","hii i am meenu parashar",(err)=>{
+//   if(err){
+//     console.log("Error");
+//   }else{
+//     console.log("File written successfully");
+//   }
+// });
+
+
+
+
+
+//append file ye purani file ka content replace nahi krta last me add kr deta h
+// const fs=require("fs");
+// fs.appendFile("notes.txt", "\n now i am learning nodejs",(err)=>{
+//   if(err){
+//     console.log("error");
+
+//   }else{
+//     console.log("file updated successfully");
+//   }
+// });
+
+
+
+//delete file
+// const fs=require("fs");
+// fs.unlink("notes.txt",(err)=>{
+//   if(err){
+//     console.log("Error"); 
+//   }else{
+//     console.log("File deleted successfully");
+//   }
+
+// });
+
+//synochronus me ye kaam seedha seedha ho jata h
+
+
+//ab ham bananyenge http se server
+// const http = require("http");
+// const server = http.createServer((req , res)=>{
+//   res.end("hello meenu");
+// });
+// server.listen(3000,()=>{
+//   console.log("server running on http://localhost:3000");
+// });
+
+// yha tak ho gya basic server
+// const http = require("http");
+// const server = http.createServer((req, res)=>{
+//   const method = req.method;
+//   const url = req.url;
+//   if(method === "GET" && uel === "/"){
+//     res.writeHead(200,{"Content-Type": "text/plain"});
+//     res.end("Welcome to Home Page");
+//   }else if(method === "GET" && url === "/user"){
+//     const users = {users: ["Alice", "Bob", "Charlie"]};
+//     res.writeHead(200,{"Content-Type":"application/json"});
+//     res.end(JSON.stringify(users));
+//   }else if(method === "POST" && url === "/users"){
+//     let body = "";
+//     req.on("data", chunk =>{
+//       body += chunk;
+//     });
+//   }
+// }); halfffff h ye abhi 
+
+
+
+
+const http = require("http");
+const server = http.createServer((req , res)=>{
+  res.end("hiii i a meenu parashar");
+});
+server.listen(3000,()=>{
+  console.log("server running on http://localhost:3000");
+});
