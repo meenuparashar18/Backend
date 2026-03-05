70ef1da9d6abb6a95995c8e59cd9418f524bfbb2
Meenu parashar <meenuparashar18@gmail.com>
2026-03-05 22:07:30 +0530

    todo

--- Assignment/Assignment1/todo.js
+++ Assignment/Assignment1/todo.js
@@ -1,24 +1,49 @@
-const fs = require("fs");
+const fs =require("fs");
+const path = require("path");
+const TODO_FILE = path.join(__dirname, "todos.json");
+console.log(TODO_FILE);
 
-const readTodo = function(){
-    const data = fs.readFileSync("./Backend/Assignment/Assignment1/todo.json" , "utf-8");
-    return JSON.parse(data);
+function readTodos(){
+  const data = fs.readFileSync(TODO_FILE, "utf-8");
+  return JSON.parse(data);
 }
-
-const writeTodos = function(newTodos){
-    const data = fs.writeFileSync("./Backend/Assignment/Assignment1/todo.json" , JSON.stringify(newTodos) );
+function writeTodos(todos){
+  fs.writeFileSync(TODO_FILE, JSON.stringify(todos));
 
 }
-
+//add todos 
 function addTodo(task){
-    const currentTodos = readTodo();
-    const newTodo = {
-        task : task,
-        id : Date.now(),
-        status : false,
-    }
-    currentTodos.push(newTodo);
-    writeTodos(currentTodos);
-}
+  const todos = readTodos();
+  const newTodo ={
+    id : Date.now(),
+    task: task,
+    done : false
 
-addTodo("Say hi to class!");
+  };
+  todos.push(newTodo);
+  writeTodos(todos);
+  console.log("Todo added:",task);
+}
+function listTodos(){
+  const todos = readTodos();
+  if(todos.length ===0){
+    console.log("No todos found.");
+    return;
+  }
+  todos.forEach((todos, index) => {
+
+    const status = todos.done ? "[x]" : "[ ]";
+    console.log(`${index + 1}. ${status} ${todos.task}`);
+  }
+});
+function markDone(index){
+  const todos = readTodos();
+  if(index <1 || index > todos.length){
+    console.log("Invalid index.");
+    return;
+  }
+  todos[index -1].done = true;
+  writeTodos(todos);
+  console.log("Todo marked as done:",todos[index -1].task);
+} 
+
