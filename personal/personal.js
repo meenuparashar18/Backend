90e9d80f6afe7c6c662ad303f147d71ecc109c0a
Meenu parashar <meenuparashar18@gmail.com>
2026-03-11 23:04:41 +0530

    Express

--- personal/personal.js
+++ personal/personal.js
@@ -562,5 +562,14 @@ app.listen(PORT, () => {
 });
 
 
+//backend ki theory
 
 
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req,res)=>{
+//   res.send("Hello");
+// });
+
+// app.listen(3000);
