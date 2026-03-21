1c12282dea77b0af440930f46e45760ae77fea14
Meenu parashar <meenuparashar18@gmail.com>
2026-03-21 21:57:28 +0530

    done

--- express/server.js
+++ express/server.js
@@ -1,18 +1,30 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// app.get("/sum",(req,res)=>{
+//     console.log(req.query.a);
+// });
+
+
+
+
+
+
+
+// app.listen(6000, () => {
+
+//     console.log("Server is running on port 6000");
+
+// });
+
+
+
+const express = require("express");
 const app = express();
-app.get("/user",(req,res)=>{
-    res.json("name: Hello World");
+app.get("/sum" , (req,res) => {
+    console.log(req.query);
+    res.send(parseInt(req.query.a) + parseInt(req.query.b));
 });
-
-
-
-
-
-
-
-app.listen(3000, () => {
-
-    console.log('Server is running on port 6000');
-
+app.listen(3000 , () =>{
+    console.log("server is running on port 3000");
 });
 
