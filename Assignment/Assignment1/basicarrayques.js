22262a32f7378e0bb508cfd34853680bd48e2447
Meenu parashar <meenuparashar18@gmail.com>
2026-03-22 16:27:52 +0530

    8. Flatten 1-level nested array

--- Assignment/Assignment1/basicarrayques.js
+++ Assignment/Assignment1/basicarrayques.js
@@ -57,18 +57,35 @@
 
 
 //7. Count element frequency (generic)
-const arr = ['a', 'b', 'a', 'c'];
+// const arr = ['a', 'b', 'a', 'c'];
 
-let freq = {};
+// let freq = {};
+
+// for (let i = 0; i < arr.length; i++) {
+//   let val = arr[i];
+
+//   if (freq[val]) {
+//     freq[val]++;
+//   } else {
+//     freq[val] = 1;
+//   }
+// }
+
+// console.log(freq);
+
+//8. Flatten 1-level nested array
+const arr = [1, [2, 3], 4];
+
+let result = [];
 
 for (let i = 0; i < arr.length; i++) {
-  let val = arr[i];
-
-  if (freq[val]) {
-    freq[val]++;
+  if (Array.isArray(arr[i])) {
+    for (let j = 0; j < arr[i].length; j++) {
+      result.push(arr[i][j]);
+    }
   } else {
-    freq[val] = 1;
+    result.push(arr[i]);
   }
 }
 
-console.log(freq);
+console.log(result);
