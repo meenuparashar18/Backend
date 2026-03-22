0d3e6c930bd49f5a02809adf5bb88152ef6e9b18
Meenu parashar <meenuparashar18@gmail.com>
2026-03-22 16:18:13 +0530

    max value

--- Assignment/Assignment1/basicarrayques.js
+++ Assignment/Assignment1/basicarrayques.js
@@ -5,9 +5,19 @@
 // }
 
 //2. Sum of array (accumulator)
+// const arr = [10,20,30,40];
+// let sum =0;
+// for(let i=0; i<arr.length; i++){
+//     sum +=arr[i];
+// }
+// console.log(sum);
+
+//3. Maximum value without Math.max
 const arr = [10,20,30,40];
-let sum =0;
-for(let i=0; i<arr.length; i++){
-    sum +=arr[i];
+let max =arr[0];
+for(let i=1; i<arr.length; i++){
+    if(arr[i]>max){
+        max=arr[i];
+    }
 }
-console.log(sum);
+console.log(max);
