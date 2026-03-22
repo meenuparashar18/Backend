59bccf8d0860912474401963b5e5b94b45fdc460
Meenu parashar <meenuparashar18@gmail.com>
2026-03-22 16:15:22 +0530

    sum of aaray

--- Assignment/Assignment1/basicarrayques.js
+++ Assignment/Assignment1/basicarrayques.js
@@ -1,4 +1,13 @@
-const arr =[10,20,30,40];
+//1. Print each element (basic iteration)
+// const arr =[10,20,30,40];
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+//2. Sum of array (accumulator)
+const arr = [10,20,30,40];
+let sum =0;
 for(let i=0; i<arr.length; i++){
-    console.log(arr[i]);
+    sum +=arr[i];
 }
+console.log(sum);
