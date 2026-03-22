897e089348ed22697db684429033c86fd5248d1d
Meenu parashar <meenuparashar18@gmail.com>
2026-03-22 16:21:15 +0530

    min value

--- Assignment/Assignment1/basicarrayques.js
+++ Assignment/Assignment1/basicarrayques.js
@@ -5,7 +5,7 @@
 // }
 
 //2. Sum of array (accumulator)
-// const arr = [10,20,30,40];
+// const arr = [1,2,3,4];
 // let sum =0;
 // for(let i=0; i<arr.length; i++){
 //     sum +=arr[i];
@@ -13,11 +13,21 @@
 // console.log(sum);
 
 //3. Maximum value without Math.max
-const arr = [10,20,30,40];
-let max =arr[0];
+// const arr = [5,1,9,3];
+// let max =arr[0];
+// for(let i=1; i<arr.length; i++){
+//     if(arr[i]>max){
+//         max=arr[i];
+//     }
+// }
+// console.log(max);
+
+//4. Minimum value with reduce
+const arr=[7,3,9,0];
+let min=arr[0];
 for(let i=1; i<arr.length; i++){
-    if(arr[i]>max){
-        max=arr[i];
+    if(arr[i]<min){
+        min=arr[i];
     }
 }
-console.log(max);
+console.log(min);
